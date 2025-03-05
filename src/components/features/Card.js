065002d793a38ce661592ea7859fb7a9eb4433d1
sourav <schatterjee.0404@gmail.com>
2025-03-05 21:07:44 +0530

    add null checks

--- src/components/features/Card.js
+++ src/components/features/Card.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import { HiArrowRight } from "react-icons/hi";
+import React from "react";
+import { BiArrowToTop } from "react-icons/bi";
 
-const Card = ({item:{title,des,icon}}) => {
+const Card = ({ item: { title, des, icon } }) => {
   return (
     <div className="w-full px-6 h-80 py-8 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors duration-100 group">
       <div className="h-72 overflow-y-hidden">
         <div className="flex h-full flex-col gap-10 translate-y-16 group-hover:translate-y-0 transition-transform duration-500">
           <div className="w-10 h-8 flex flex-col justify-between">
-        
             {icon ? (
               <span className="text-5xl text-designColor">{icon}</span>
             ) : (
@@ -24,14 +23,14 @@ const Card = ({item:{title,des,icon}}) => {
               {title}
             </h2>
             <p className="base">{des}</p>
-            <span className="text-2xl text-designColor">
-              <HiArrowRight />
+            <span className="flex justify-center text-2xl text-designColor">
+              <BiArrowToTop />
             </span>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Card
+export default Card;
